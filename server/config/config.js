15d8cd3a4c68326f66d4885e6f0b058b83d59f06
Jorge Acero <julianacero98@gmail.com>
2019-02-17 09:39:21 -0600

    Security added

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:jjalcs5x17798@ds045882.mlab.com:45882/cafe'
+    urlDB = MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
