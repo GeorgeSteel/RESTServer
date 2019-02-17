e1962f346d06b48602e2a7cb46aac8d9a4f8a3fd
Jorge Acero <julianacero98@gmail.com>
2019-02-17 09:15:56 -0600

    mlab database connected

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,16 @@
 // Puerto
-
 process.env.PORT = process.env.PORT || 3000;
+
+// Environment
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// DB
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:jjalcs5x17798@ds045882.mlab.com:45882/cafe'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use(require('./routes/user'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
     if(err) throw err;
     console.log('mongoDB Connected');
 });
